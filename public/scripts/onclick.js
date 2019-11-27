a1b02272e92a191a91e68854960f853a9b6f8927
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-11-27 18:24:54 +0800

    added buttons

---
+++ public/scripts/onclick.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+
+    $('#showManual1').click(function () {
+        $('#manual1').toggle();
+    });
+
+    $('#showManual2').click(function () {
+        $('#manual2').toggle();
+    });
+
+    $('#showManual3').click(function () {
+        $('#manual3').toggle();
+    });
+
+    $('#showManual4').click(function () {
+        $('#manual4').toggle();
+    });
+
+    $('#showManual5').click(function () {
+        $('#manual5').toggle();
+    });
+
+    $('#showManual6').click(function () {
+        $('#manual6').toggle();
+    });
+
+    $('#showManual7').click(function () {
+        $('#manual7').toggle();
+    });
+
+    $('#showManual8').click(function () {
+        $('#manual8').toggle();
+    });
+
+    $('#showManual9').click(function () {
+        $('#manual9').toggle();
+    });
+
+    $('#showManual10').click(function () {
+        $('#manual10').toggle();
+    });
+
+    $('#showManual11').click(function () {
+        $('#manual11').toggle();
+    });
+
+    $('#showManual12').click(function () {
+        $('#manual12').toggle();
+    });
+    $('#showManual13').click(function () {
+        $('#manual13').toggle();
+    });
+
+    $('#showManual14').click(function () {
+        $('#manual14').toggle();
+    });
+
+    $('#showManual15').click(function () {
+        $('#manual15').toggle();
+    });
+
+    $('#showManual16').click(function () {
+        $('#manual16').toggle();
+    });
+
+    $('#showManual17').click(function () {
+        $('#manual17').toggle();
+    });
+})
